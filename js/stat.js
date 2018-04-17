f8b11ce5edc815a46cdf948b6d6ec594452b0f94
dbogdanov <dbogdanov87@yandex.ru>
2018-04-17 12:11:26 +0300

    Добавил функцию генерации цвета по имени игрока для столбцов

--- js/stat.js
+++ js/stat.js
@@ -1,16 +1,16 @@
 'use strict';
 
+var BAR_WIDTH = 40;
 var CLOUD_WIDTH = 420;
 var CLOUD_HEIGHT = 270;
 var CLOUD_X = 100;
 var CLOUD_Y = 10;
-var GAP = 10;
 var FOUNT_GAP = 15;
+var GAP = 10;
 var GAP_X_TEXT_VICTORY = 30;
 var GAP_Y_TEXT_VICTORY = 30;
 var TEXT_WIDTH = 50;
-var BAR_WIDTH = 40;
-var heightHistogram = 150;
+var USER_NAME = 'Вы';
 
 
 var getMaxElement = function (arr) {
@@ -21,6 +21,15 @@ var getMaxElement = function (arr) {
       maxElement = arr[i];
     }
   } return maxElement;
+};
+
+var colorizeBarsByName = function (name) {
+  var barColor = 'blue';
+  if (name === USER_NAME) {
+    barColor = 'rgba(255, 0, 0, 1)';
+  } else {
+    barColor = 'rgba(30, 27, 94, ' + Math.random();
+  } return barColor;
 };
 
 var renderCloud = function (ctx, x, y, color) {
@@ -43,14 +52,17 @@ window.renderStatistics = function (ctx, names, times) {
 
   // отрисовка гистограммы
   var maxTime = getMaxElement(times);
+  var heightHistogram = 150;
   for (var i = 0; i < names.length; i++) {
     // вывод имен игроков
     ctx.fillText(names[i], CLOUD_X + GAP + FOUNT_GAP + (GAP + TEXT_WIDTH + BAR_WIDTH) * i,
         CLOUD_HEIGHT - GAP);
     // вывод столбцов с результатами
+    ctx.fillStyle = colorizeBarsByName(names[i]);
     ctx.fillRect(CLOUD_X + GAP + FOUNT_GAP + (GAP + TEXT_WIDTH + BAR_WIDTH) * i,
         CLOUD_HEIGHT - CLOUD_Y - FOUNT_GAP,
         BAR_WIDTH, -(heightHistogram * times[i]) / maxTime);
+    ctx.fillStyle = 'rgba(0, 0, 0, 1)'; // возвращаю черный цвет
     // вывод времени
     ctx.fillText(Math.round(times[i]), CLOUD_X + GAP + FOUNT_GAP + (GAP + TEXT_WIDTH + BAR_WIDTH) * i,
         CLOUD_HEIGHT - FOUNT_GAP - CLOUD_Y - FOUNT_GAP - (heightHistogram * times[i]) / maxTime);
