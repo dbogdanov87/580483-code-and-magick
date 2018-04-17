47ad308f9f618c2229a94e9d3dc63f86acd30d77
dbogdanov <dbogdanov87@yandex.ru>
2018-04-17 11:51:06 +0300

    Добавил файл stat.js и создал там прямоуголное облако. Подключил stat.js в index.html

---
+++ js/stat.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var CLOUD_WIDTH = 420;
+var CLOUD_HEIGHT = 270;
+var CLOUD_X = 100;
+var CLOUD_Y = 10;
+var GAP = 10;
+
+
+var renderCloud = function (ctx, x, y, color) {
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
+};
+
+window.renderStatistics = function (ctx) {
+  renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7');
+  renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
+};
+
